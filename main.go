e59dbf08739fe53d997427964191b800733d5b03
Zach Goldstein <zachgold@gmail.com>
2015-02-12 11:43:43 +1100

    Added endpoints to return 500 errors, validation errors and a mix of both
    - Added a 500 error-returning endpoint
    - Added an endpoint to return 200's with responses that do no validate
    - Added an endpoint to mix validation error responses with 500 error responses
    
    Impact: Critical

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"net/http"
 	"fmt"
 	"log"
+	"time"
+	"math/rand"
+	"errors"
 )
 
 func main() {
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/test/success", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write( []byte(responseDocument) )
+
+		duration := time.Millisecond * time.Duration(3000 * rand.Float64())
+		fmt.Println("sleeping for ",duration)
+		time.Sleep(duration)
 
 		if (err != nil){
 			fmt.Printf("An error occurred writing the document %v", err)
@@ -17,7 +24,61 @@ func main() {
 		fmt.Printf("Returned a doc %v", responseDocument)
 	})
 
-	fmt.Printf("Serving on port 8080 route /test")
+	http.HandleFunc("/test/fail/validate", func(w http.ResponseWriter, r *http.Request) {
+		errorChance := 0.50
+		var err error
+		if errorChance > rand.Float64() {
+			_, err = w.Write( []byte(responseDocument) )
+		} else {
+			_, err = w.Write( []byte(failResponseDocument) )
+			return
+		}
+		if (err != nil){
+			fmt.Errorf("An error occurred writing the document %v", err)
+		}
+
+		duration := time.Millisecond * time.Duration(3000 * rand.Float64())
+		fmt.Println("sleeping for ",duration)
+		time.Sleep(duration)
+
+		fmt.Printf("Returned a doc %v", responseDocument)
+	})
+
+	http.HandleFunc("/test/fail/error", func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		err = errors.New("Testing error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Errorf("A testing error is returing %v", err)
+
+		duration := time.Millisecond * time.Duration(3000 * rand.Float64())
+		fmt.Println("sleeping for ",duration)
+		time.Sleep(duration)
+
+		fmt.Printf("Returned a doc %v", responseDocument)
+	})
+
+	http.HandleFunc("/test/fail/mix", func(w http.ResponseWriter, r *http.Request) {
+		errorChance := 0.50
+		var err error
+		if errorChance > rand.Float64() {
+			err = errors.New("Testing error")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			fmt.Errorf("A testing error is returing %v", err)
+			return
+		} else {
+			_, err = w.Write( []byte(failResponseDocument) )
+			fmt.Errorf("An error occurred writing the document %v", err)
+			return
+		}
+
+		duration := time.Millisecond * time.Duration(3000 * rand.Float64())
+		fmt.Println("sleeping for ",duration)
+		time.Sleep(duration)
+
+		fmt.Printf("Returned a doc %v", responseDocument)
+	})
+
+	fmt.Printf("Serving on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -27,6 +88,16 @@ var responseDocument = `
     "name": "A green door",
     "stringNumber": "3",
     "price": 12.50,
+    "tags": ["home", "green"]
+}
+`
+
+var failResponseDocument = `
+{
+    "id": 1,
+    "name": "A green door",
+    "stringNumber": "wtf",
+    "price": "this should never happen!!!!",
     "tags": ["home", "green"]
 }
 `
